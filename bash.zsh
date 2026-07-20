b9fead821399f381774c072495c740526611eddc
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-20 07:57:15 +0100

    Create bash.zsh

---
+++ bash.zsh
@@ -0,0 +1,8 @@
+-d
+npm install libomemo.js
+-m git clone git@github.com:conversejs/converse.js.git
+cd converse.js
+-m git clone https://github.com/conversejs/media.git media; // To see images like sponsorship logos
+npm install
+npm run build
+npm run serve -- -p 8008
